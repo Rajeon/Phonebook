e8430e1ff0a3f1e6f13d843a1e2d32fbc90b8e5e
Ray Felton <frajeon@gmail.com>
2019-10-23 19:58:16 -0400

    Finshed Work

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,64 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, List<String>> map;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new HashMap<String, List<String>>());
     }
 
     public void add(String name, String phoneNumber) {
+        ArrayList<String> phonebook = new ArrayList();
+        phonebook.add(phoneNumber);
+        map.put(name, phonebook);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        ArrayList<String> phonebook = new ArrayList<>();
+        phonebook.addAll(Arrays.asList(phoneNumbers));
+        map.put(name, phonebook);
+
+
     }
 
     public void remove(String name) {
+        ArrayList<String> phonebook = new ArrayList<>();
+        phonebook.remove(name);
+        map.put(name, phonebook);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.containsKey(name);
     }
 
     public List<String> lookup(String name) {
+        return map.get(name);
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        List<String> phonebook = new ArrayList<>();
+        for (String name : map.keySet()) {
+            phonebook = lookup(name);
+            if (phonebook.contains(phoneNumber))
+                return name;
+        }
         return null;
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
-    }
-
-    public List<String> getAllContactNames() {
-        return null;
+    public String getAllContactNames() {
+        return String.valueOf(map);
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.map;
     }
 }
